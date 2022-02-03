5508b0e1f8b531fd1c7f96bab975ccc2fe979cdb
Tagbug <tagbuged@gmail.com>
2022-02-03 19:51:10 +0800

    feat: 接入图片上传接口 fix: 文章发布页优化

--- src/services/upload.js
+++ src/services/upload.js
@@ -6,6 +6,18 @@ export function uploadImage(option) {
     return request({
         method: 'POST',
         url: '/upload/image',
+        data: option,
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+}
+
+// 撤销图片上传
+export function revertUploadImage(option) {
+    return request({
+        method: 'POST',
+        url: '/upload/image/revert',
         data: option
     })
 }
